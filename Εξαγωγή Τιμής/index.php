13ef5066c20ea6edc696616cefb19efbb48f4577
KonstantinosPoulianidis <67283948+KonstantinosPoulianidis@users.noreply.github.com>
2023-06-15 22:44:26 +0300

    updated

--- Εξαγωγή Τιμής/index.php
+++ Εξαγωγή Τιμής/index.php
@@ -9,7 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($username == $desiredUsername && $password == $desiredPassword) {
         // Αν η είσοδος είναι επιτυχής, ανακατεύθυνση στη σελίδα του YouTube
-        header("Location: http://127.0.0.1:5500/%CE%9A%CE%95%CE%9D%CE%A4%CE%A1%CE%99%CE%9A%CE%97%20%CE%A3%CE%95%CE%9B%CE%99%CE%94%CE%91/%CE%95%CE%BE%CE%B1%CE%B3%CF%89%CE%B3%CE%AE%20%CE%A4%CE%B9%CE%BC%CE%AE%CF%82/%CE%95%CE%AF%CF%83%CE%BF%CE%B4%CE%BF%CF%82%20%CF%83%CF%84%CE%B7%20%CF%83%CE%B5%CE%BB%CE%AF%CE%B4%CE%B1/index2.html");
+        header("Location: https://konstantinospoulianidis.github.io/FormPrivateLabel/");
         exit();
     } else {
         echo "Λανθασμένο όνομα χρήστη ή κωδικός πρόσβασης.";
